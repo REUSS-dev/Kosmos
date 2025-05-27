460136cb3151246d00d44e9734585eb7aa6ca472
qq <mreu@dr.com>
2025-05-27 17:39:40 +0700

    docs

--- common/classes/KosmoApi.lua
+++ common/classes/KosmoApi.lua
@@ -38,17 +38,21 @@ end
 
 ---@class KosmoApi Versioned API object
 ---@field private path string Path to folder with API realizations
----@field private defaultVersion ApiVersion Fallback version if unknown API version is given
+---@field private defaultVersion ApiVersion Fallback version if unknown API version is queried
 ---@field public v {default: ApiVersion, [ApiVersion]: table<ApiMethodName, function>} Table with all version realizations
 ---@field private scope table<ApiMethodName, ApiScopeGroup> Method scopes.
----@field private fallback table<ApiVersion, ApiVersion> Map of loaded versions (keys) to their fallback versions (values)
+---@field private fallback table<ApiVersion, ApiVersion?> Map of loaded versions (keys) to their fallback versions (values)
 local KosmoApi = {}
 local KosmoApi_meta = { __index = KosmoApi }
 
+---Returns default API version.
+---@return ApiVersion
 function KosmoApi:getDefaultVersion()
     return self.defaultVersion
 end
 
+---Sets default API version
+---@param new_default integer Version to set as default for this API
 function KosmoApi:setDefaultVersion(new_default)
     self.defaultVersion = new_default
     self.v.default = new_default
@@ -58,6 +62,7 @@ end
 ---@param version integer
 function KosmoApi:cacheVersion(version)
     local realization = self.v[version]
+    assert(realization, "Attempt caching undefined version. API " .. self.path .. " does not have version " .. tostring(version))
 
     local previous_version = self.fallback[version]
 
@@ -72,19 +77,22 @@ function KosmoApi:cacheVersion(version)
     end
 end
 
----Checks whether provided method satisfies scope limitations of provided scope map
----@param method ApiMethodName
----@param token KosmoToken?
+---Checks whether provided method satisfies scope limitations of provided token
+---@param method ApiMethodName Method to check scope for
+---@param token KosmoToken? Token object defining scopes. Can be nil, only guest access is permited in this case.
 ---@return boolean? scope_status true, if method is within provided token's scope; false, if it is outside of provided token's scope; nil, if such method does not exist
 function KosmoApi:isMethodWithinScope(method, token)
+    -- Guest scope does not depend on token
     if self.scope[method] == SCOPE_GUEST then
         return true
     end
 
+    -- Token not provided to a non-guest method
     if not token then
         return false
     end
 
+    -- Scope for method is not defined, method does not exist
     if not self.scope[method] then
         return nil
     end
@@ -92,17 +100,22 @@ function KosmoApi:isMethodWithinScope(method, token)
     return token:checkScope(self.scope[method])
 end
 
-function KosmoApi:init()
-    local items = love.filesystem.getDirectoryItems(self.path)
+---Read API files from provided folder path
+---@param folder string? Path to folder with API version files (defaults to self.path)
+---@return integer
+function KosmoApi:readAPIs(folder)
+    folder = folder or self.path
 
     local last
+
+    local items = love.filesystem.getDirectoryItems(self.path)
 
     for _, version_filename in ipairs(items) do
         local parsed_version = string.match(version_filename, VERSION_PATTERN)
 
         if parsed_version then -- another api version file
             local version = tonumber(parsed_version) --[[@as integer]]
-            local realization_chunk = love.filesystem.load(self.path .. "/" .. version_filename)
+            local realization_chunk = love.filesystem.load(folder .. "/" .. version_filename)
 
             local realization, fallback_version = realization_chunk()
 
@@ -117,9 +130,18 @@ function KosmoApi:init()
         end
     end
 
+    return last
+end
+
+---Initialize newly created KosmoApi objects
+---@package
+function KosmoApi:init()
+    -- Read APIs
+    local last = self:readAPIs()
+
     assert(self.scope, "API methods scope file not found for api at " .. self.path)
 
-    self.defaultVersion = self.defaultVersion or last
+    self:setDefaultVersion(last)
 
     for version, realization in pairs(self.v) do
         if version ~= "default" then
@@ -136,20 +158,20 @@ function KosmoApi:init()
     if AUTO_CACHE_LAST then
         self:cacheVersion(last)
     end
-
-    self:setDefaultVersion(self.defaultVersion)
-
-    setmetatable(self.v, { __index = version_defaulter })
 end
 
 -- kosmoapi fnc
 
+---Create new KosmoApi object using path to API versions directory
+---@param api_path string Path to API realization files.
+---@return KosmoApi
 function kosmoapi.new(api_path)
     assert(api_path, "API path not provided!")
 
     local obj = setmetatable({ path = api_path, v = {}, fallback = {}}, KosmoApi_meta)
-
     obj:init()
+
+    setmetatable(obj.v, { __index = version_defaulter })
 
     return obj
 end
--- common/classes/KosmoToken.lua
+++ common/classes/KosmoToken.lua
@@ -33,39 +33,41 @@ local token = {}
 --#region KosmoToken
 
 ---@class KosmoToken
----@field token_bytes TokenString Raw token bytes string
+---@field token_bytes TokenString Raw token binary string
 ---@field scope table<TokenScope, boolean> Token scope map
----@field owner HostPeerIndex IP address owning the token
----@field clid integer ID of a user who owns this token
+---@field owner HostPeerIndex Index of a peer owning the token
+---@field clid integer ID of a user who this token corresponds to
 local KosmoToken = {}
 local KosmoToken_meta = { __index = KosmoToken }
 
+---Returns binary string representation of a token.
+---@return TokenString
 function KosmoToken:getToken()
     return self.token_bytes
 end
 
 ---Checks if provided scope group within this token's scope
----@param group TokenScope
----@return boolean
+---@param group TokenScope API scope group string
+---@return boolean check True, if this token covers this scope group, false otherwise
 function KosmoToken:checkScope(group)
     return self.scope[group] or false
 end
 
----Checks if provided IP is a token's owner
----@param peer_index HostPeerIndex
----@return boolean
+---Checks if provided peer index is a token's owner
+---@param peer_index HostPeerIndex Index of a peer to check against ownership of this token
+---@return boolean check True, if provided index is this token's owner, false otherwise
 function KosmoToken:checkOwner(peer_index)
     return self.owner == peer_index
 end
 
----Returns owner of a token
----@return HostPeerIndex
+---Returns owner of this token
+---@return HostPeerIndex peer_index Index of a peer owning this token
 function KosmoToken:getOwner()
     return self.owner
 end
 
 ---Returns client id of a token
----@return integer
+---@return integer clid Internal system client ID this token corresponds to
 function KosmoToken:getClientID()
     return self.clid
 end
@@ -74,37 +76,45 @@ end
 
 --#region KosmoTokenCollection
 
----@class KosmoTokenCollection
----@field tokens table<TokenString, KosmoToken>
+---@class KosmoTokenCollection Advanced KosmoToken container.
+---@field tokens table<TokenString, KosmoToken> Collection of tokens in this KosmoTokenCollection
 local KosmoTokenCollection = {}
 local KosmoTokenCollection_meta = { __index = KosmoTokenCollection }
 
----Add a token to a token collection
+---Add a token to this token collection
 ---@param new_token KosmoToken
 function KosmoTokenCollection:add(new_token)
     self.tokens[new_token:getToken()] = new_token
 end
 
+---Search token in this collection by binary string representation of the token.
+---@param token_string string
+---@return KosmoToken?
 function KosmoTokenCollection:find(token_string)
     return self.tokens[token_string]
 end
 
 ---Delete token from collection
----@param token_or_string KosmoToken|TokenString
+---@param token_or_string KosmoToken|TokenString Binary representation or token object itself.
 function KosmoTokenCollection:delete(token_or_string)
+    -- Может быть легко упрощен до:
+    -- token_or_string = token_or_string.token_byrtes or token_or_string
+    -- Но это не очень интуитивно
     if type(token_or_string) == "string" then
         self.tokens[token_or_string] = nil
+
     elseif type(token_or_string) == "table" then
         if token_or_string.token_bytes then
             self.tokens[token_or_string.token_bytes] = nil
         end
+
     else
         error("Arg 1 to KosmoTokenCollection:delete() string or KosmoToken expected, got " .. type(token_or_string))
     end
 end
 
----Deletes all tokens associated with given peer
----@param peer HostPeerIndex
+---Deletes all tokens within this collection associated with given peer
+---@param peer HostPeerIndex Index of a peer to filter tokens
 function KosmoTokenCollection:deletePeerTokens(peer)
     for token_bytes, t in pairs(self.tokens) do
         if t:checkOwner(peer) then
@@ -118,11 +128,11 @@ end
 -- token fnc
 
 ---Create new token object
----@param tokenStr string Token bytes
----@param scope table<TokenScope, boolean>
----@param owner HostPeerIndex
----@param clid integer
----@return KosmoToken
+---@param tokenStr string Binary string of a token
+---@param scope table<TokenScope, boolean> Scope map of a token
+---@param owner HostPeerIndex Index of a peer owning a token
+---@param clid integer Client ID of a new token
+---@return KosmoToken new_KosmoToken New KosmoToken object
 function token.new(tokenStr, scope, owner, clid)
     local new_token = setmetatable({
         token_bytes = tokenStr,
@@ -134,6 +144,8 @@ function token.new(tokenStr, scope, owner, clid)
     return new_token
 end
 
+---Create new empty Token collection
+---@return KosmoTokenCollection new_KosmoTokenCollection New KosmoTokenCollection object
 function token.newCollection()
     local new_collection = setmetatable({
         tokens = {}
